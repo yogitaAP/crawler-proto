5d22a8755fc057cb2be9a17cab35f94765778aa0
vagrant <vagrant@precise32.(none)>
2015-08-20 10:20:13 +0000

    Added file spirit-of-age.py to calc folder

---
+++ lib/helper/calcs/spirit-of-age.py
@@ -0,0 +1,8 @@
+# Write python code that defines the variable
+# age to be your age in years, and then prints
+# out the number of days you have been alive.
+# Assume there are 365 days in year, leap year
+
+age = 20
+days_I_lived = age * 365
+print days_I_lived
