3ca4237ee026f003d6741535fd75bf8bd94c534f
yogitaAP <yogita088sharma@gmail.com>
2016-01-12 07:03:30 +0000

    Added list-nested-access.py

---
+++ lib/helper/lists/list-nested-access.py
@@ -0,0 +1,16 @@
+#Given the variable conuntries defined as:
+
+#Name, Capital Population(M)
+
+countries = [['China','Beijing',1350],
+             ['India','Delhi',1210],
+             ['Romania','Bucharest',21],
+             ['United States','Washington',307]] 
+
+
+# What multiple of Romania's population is the population
+# of China?
+
+multiple = countries[0][2] / countries[2][2]
+
+print multiple
