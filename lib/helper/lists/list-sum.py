49966d626232cdbe937f88602449a759c66bb7f7
yogitaAP <yogita088sharma@gmail.com>
2016-01-14 16:50:39 +0000

    Added file list-sum.py

---
+++ lib/helper/lists/list-sum.py
@@ -0,0 +1,11 @@
+#This procedure takes a list of numbers as input 
+#and returns the sum of all elemensts in the list
+
+def sum_list(nums):
+	total=0
+	for n in nums:
+		total += n
+
+	return total
+	
+		
