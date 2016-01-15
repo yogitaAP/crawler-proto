cfab04e19ff6b056e00a06f50c8ea4569ace8507
yogitaAP <yogita088sharma@gmail.com>
2016-01-15 18:37:21 +0000

    Added menthod union definition

--- lib/helper/lists/list-union.py
+++ lib/helper/lists/list-union.py
@@ -6,18 +6,36 @@
 # is a set, that is, it contains no 
 # repeated elements.
 
+def find_element_while(p, t):
+	i = 0
+	while i < len(p):
+		if p[i] == t:
+			return i;
+		i = i + 1
+	return -1	
 
 
-
+def union(p,q):
+    for e in q:
+        index = find_element_while(p,e)
+        if index == -1:
+            p.append(e)
+            
 
 
 # To test, uncomment all lines 
 # below except those beginning with >>>.
 
-#a = [1,2,3]
-#b = [2,4,6]
-#union(a,b)
-#print a 
+a = [1,2,3]
+b = [2,4,6]
+union(a,b)
+print a 
 #>>> [1,2,3,4,6]
-#print b
+print b
 #>>> [2,4,6]
+
+
+
+
+
+
