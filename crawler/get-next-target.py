88d7304b11815f42c34aeec9ad7377f75c66a1e1
yogitaAP <yogita088sharma@gmail.com>
2016-01-16 07:32:16 +0000

    Added method get_All_links

--- crawler/get-next-target.py
+++ crawler/get-next-target.py
@@ -22,3 +22,13 @@ def get_next_target(page):
     url = page[start_quote + 1:end_quote]
     return url, end_quote
 
+def get_all_links(page):
+    links = []
+    while True:
+        url, endpos =  get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break;
+    return links
