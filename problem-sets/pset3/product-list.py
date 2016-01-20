a2fe03eaa99dd966d31363c93a906333ffe2c2a7
yogitaAP <yogita088sharma@gmail.com>
2016-01-20 19:38:44 +0000

    Added problem-sets folder

---
+++ problem-sets/pset3/product-list.py
@@ -0,0 +1,25 @@
+# Procedure, product_list,
+# that takes as input a list of numbers,
+# and returns a number that is
+# the result of multiplying all
+# those numbers together.
+
+def product_list(list_of_numbers):
+	product = 1
+	for number in list_of_numbers:
+		product = product * number
+	return product
+
+
+
+
+
+
+print product_list([9])
+#>>> 9
+
+print product_list([1,2,3,4])
+#>>> 24
+
+print product_list([])
+#>>> 1
