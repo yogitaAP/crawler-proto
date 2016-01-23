37644547403c5aa95316988a9aeab3839b1c12ef
yogitaAP <yogita088sharma@gmail.com>
2016-01-23 09:47:03 +0000

    Added depth param

--- crawler/crawl-web-r.py
+++ crawler/crawl-web-r.py
@@ -68,14 +68,19 @@ def get_all_links(page):
             break
     return links
 
-def crawl_web(seed, max_pages):
+def crawl_web(seed, max_pages, max_depth):
     tocrawl = [seed]
     crawled = []
-    while tocrawl:
+    next_depth = []
+    depth = 0
+    while tocrawl and depth < max_depth:
         page = tocrawl.pop()
-        if page not in crawled and len(crawled) < max_pages:
-            union(tocrawl, get_all_links(get_page(page)))
+        if page not in crawled:
+            union(next_depth, get_all_links(get_page(page)))
             crawled.append(page)
+        if not tocrawl:
+            tocrawl, next_depth = next_depth, []
+            depth = depth + 1    
     return crawled
 
 print crawl_web("http://www.udacity.com/cs101x/index.html",1) 
